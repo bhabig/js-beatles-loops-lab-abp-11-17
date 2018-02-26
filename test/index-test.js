7cbc556eca72d501c12acd2524bbe729996c5a8c
Bob Habig <rfhab2@gmail.com>
2018-02-26 17:55:48 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,7 +3,7 @@ describe('Beatles Loops', function() {
     const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-    expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
+    expect().toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George theBeatlesPlay(musicians, instruments)Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
   });
 
   describe('johnLennonFacts', function(){
